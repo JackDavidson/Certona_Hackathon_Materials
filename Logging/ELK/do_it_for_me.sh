eee8aec0c76c6efd8e0e6f43ef5391fb2a7567b6
Jack Davidson <jack.davidson@cox.net>
2018-01-06 15:58:00 -0800

    initial commit

---
+++ Logging/ELK/do_it_for_me.sh
@@ -0,0 +1,16 @@
+ELASTIC_SEARCH_IP='15.15.0.2'
+
+echo 'creating a subnet for docker at 15.15.0.0/16'
+sudo docker network create --subnet=15.15.0.0/16 esnetwork
+
+echo 'starting up elastic search'
+sudo docker run --net esnetwork --ip $ELASTIC_SEARCH_IP --name=elastic_search_for_certona -d -e ELASTIC_PASSWORD=ElasticPass docker.elastic.co/elasticsearch/elasticsearch-oss:6.1.1 > /dev/null 2>/dev/null
+echo 'elastic search should be up and running.'
+echo
+echo
+echo 'starting up kibana oss (without X-Pack)'
+sudo docker run -p 5601:5601 --net esnetwork --name=kibana_for_certona -d -e ELASTICSEARCH_URL='http://'$ELASTIC_SEARCH_IP':9200' -e ELASTICSEARCH_PASSWORD='ElasticPass' docker.elastic.co/kibana/kibana-oss:6.1.1 > /dev/null 2>/dev/null
+echo 'kibana should be up and running.'
+echo
+echo 'running docker processes:'
+sudo docker ps

3d39c98a8f2c108d8b5c3282fe6d31d688dec905
Ben Abrams <babrams@certona.com>
2018-01-30 15:01:09 -0800

    removed backgrounding on docker run commands.

--- Logging/InfluxDB_Grafana/do_it_for_me.sh
+++ Logging/InfluxDB_Grafana/do_it_for_me.sh
@@ -17,18 +17,18 @@ sudo chmod 777 $workdir/telegraf
 echo 'Building influxdb default config'
 sudo docker run --rm --net esnetwork --ip $INFLUXDB_IP --name=influxdb_for_certona -p 8083:8083 -p 8086:8086 -v $workdir/influxdb/:/var/lib/influxdb influxdb influxd config > $workdir/influxdb/influxdb.conf
 echo 'starting influxdb under docker'
-sudo docker run --net esnetwork --ip $INFLUXDB_IP --name=influxdb_for_certona -p 8083:8083 -p 8086:8086 -v $workdir/influxdb/:/var/lib/influxdb -v $workdir/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf influxdb > /dev/null 2>/dev/null &
+sudo docker run --net esnetwork --ip $INFLUXDB_IP --name=influxdb_for_certona -p 8083:8083 -p 8086:8086 -v $workdir/influxdb/:/var/lib/influxdb -v $workdir/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf influxdb > /dev/null 2>/dev/null
 echo 'influxdb should be up and running'
 echo
 echo 'Building telegraf default config'
 sudo docker run --rm --net esnetwork --ip $TELEGRAF_IP --name=telegraf_for_certona --link influxdb_for_certona telegraf -sample-config > $workdir/telegraf/telegraf.conf
 sudo sed -i 's/127.0.0.1/influxdb_for_certona/g' $workdir/telegraf/telegraf.conf
 echo 'starting telegraf under docker'
-sudo docker run --net esnetwork --ip $TELEGRAF_IP --name=telegraf_for_certona -v $workdir/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf --link influxdb_for_certona telegraf > /dev/null 2>/dev/null &
+sudo docker run --net esnetwork --ip $TELEGRAF_IP --name=telegraf_for_certona -v $workdir/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf --link influxdb_for_certona telegraf > /dev/null 2>/dev/null
 echo 'telegraf should be up and running'
 echo
 echo 'starting grafana under docker'
-sudo docker run --net esnetwork --ip $GRAFANA_IP --name=grafana_for_certona -d -p 3000:3000 -v $workdir/grafana/:/var/lib/grafana --link influxdb_for_certona grafana/grafana > /dev/null 2>/dev/null &
+sudo docker run --net esnetwork --ip $GRAFANA_IP --name=grafana_for_certona -d -p 3000:3000 -v $workdir/grafana/:/var/lib/grafana --link influxdb_for_certona grafana/grafana > /dev/null 2>/dev/null
 echo 'grafana should be up and running'
 echo
 sleep 3
